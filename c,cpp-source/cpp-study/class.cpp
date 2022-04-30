20dd46ea4fb2fba0c29fbfdd976ae9480aafa811
taehyoen-park <pjbae2007@gmail.com>
2022-05-01 00:52:49 +0900

    language-study

--- c,cpp-source/cpp-study/class.cpp
+++ c,cpp-source/cpp-study/class.cpp
@@ -1,25 +1,38 @@
 #include<iostream>
+#include<vector>
 
 using namespace std;
 
-class Champion{
+class Animal{
 private:
-	int MP,MP,moving_speed;
-	int attack,attack_speed,defens;
+	string name,species;
+	int age,weight,hunger;
 public:
-	int moving(int distance)
-	int Toattack(int attack)
-	int Skill(int MP)
-	int damage(int HP);
+	Animal(string species = "없음",string name = "없음");
+	int get_age()const{return age;}
+	int get_weight()const{return weight;}
+	string get_name()const{return name;}
+	string get_species()const{return species;}
+	void set_age(int age){this->age = age;}
+	void set_weight(int weight){this->weight = weight;}
+	void set_name(string name){this->name = name;}
+	void set_species(string species){this->species = species;}
+	void feed(int food){this->hunger += food;}
 };
+
+Animal::Animal(string species,string name)
+{
+	age = 0;
+	hunger = 100;
+	weight = 10;
+	this->name = name;
+	this->species = species;
+}
+
 int main()
 {
-	Champion new[30];
-	
-	cout << "1.챔피언 생성" << endl;
-	cout << "2.게임 시작" << endl;
-	cout << "3.게임 종료" << endl;
-	 
-	
+	Animal * dog = new Animal[3];
+	cout << dog[0].get_name() << endl;
+
 	return 0;
-} 
+}
---
+++ c,cpp-source/cpp-study/test.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    cout << "? ?Žˆ?–­? ?ŽŒ?½ï¿½ëª´? ï¿? ? ?ŽŒë¿?? ?ŽŒ? ¾? ?Žˆë¦?? ?Žˆë»»å ?ŽŒê¶?:";
+    cin >> a;
+    cout << a << endl;
+    
+    return 0;
+}
