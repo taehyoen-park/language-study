75aae478146caec251d1ddcbcd118c4bf20e0450
tae hyeon <pjbae2007@gmail.com>
2023-04-08 03:26:24 +0900

    modi

---
+++ java_source/Calculators.java
@@ -0,0 +1,30 @@
+package java_source;
+
+public class Calculators {
+
+    public static void main(String arg[])
+    {
+        
+    }
+    public int add(int a,int b)
+    {
+        return a + b;
+    }
+
+    public int sub(int a, int b)
+    {
+        return a-b;
+    }
+    
+    public int mul(int a, int b)
+    {
+        return a*b;
+    }
+
+    public int div(int a, int b)
+    {
+        return a/b;
+    }
+
+
+}
--- java_source/Helloworld.java
+++ java_source/Helloworld.java
@@ -1,14 +1,30 @@
 package java_source;
-import java.util.Random;
+import java.util.*;
 
 public class Helloworld{
     public static void main(String[] args)
     {
-        Random rand = new Random();
-        System.out.println("랜덤 숫자" + rand.nextInt(10));
-        System.out.print("Helloworld");
-        
-        
-    }
 
+        Calculators calculators = new Calculators();
+        int a = 2;
+        Integer b = 3;
+        System.out.println(a+b);
+        System.out.println(calculators.add(a,b));
+        System.out.println(calculators.div(a, b));
+        System.out.println(calculators.mul(a, b));
+        System.out.println(calculators.sub(a, b));
+
+        String name = "taehyton";
+        name  = "태현";
+
+        
+
+
+
+
+        
+        
+
+    }
+    
 }
